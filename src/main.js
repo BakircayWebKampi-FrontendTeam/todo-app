19529546b25066ba38ae0de322efd3b24bfe7f26
Bahadır Tuna Aydın <aydinbahadirtuna@gmail.com>
2022-08-12 18:57:10 +0300

    bazı düzeltmeler yapıldı

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import { createApp } from "vue";
 import { createStore } from "vuex";
 import App from "./App.vue";
+import "@/styles/main.css";
 
 const store = createStore({
   state: {
     todos: [],
   },
-  getters: {},
 });
 
 createApp(App).use(store).mount("#app");
